078f720be42f9bc6aa6c7c6e320502a1c82426d0
David Castro <david.castro@unitec.edu>
2015-03-27 14:48:35 -0600

    EDISLON HISTORIAL

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@
 #include "productos.h"
 #include "lujo.h"
 #include "alcoholicos.h"
+
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+
 int Edilson_Menu();
 int Edilson_PMenu();
+void Edilson_Historial(vector<Productos*>);
+
 int main(int argc, char *argv[])
 {
+    cout<<"¡Bienvenido Edilson!"<<endl;
 	vector<Productos*> productos;
     double edilson_educativo, edilson_alcoholico, edilson_delujo;
     cout << "Tasa de productos educativos: " ;
@@ -67,6 +72,14 @@ int main(int argc, char *argv[])
     				break;
     		}
     		break;
+            case 3 : {
+                Edilson_Historial(vector<Productos*> productos);
+            }
+            break;
+            case 4:{
+                break;
+            }
+            break;
     }
 
 }
@@ -75,13 +88,15 @@ int Menu(){
 	int edilson_temp;
 	cout << "1. Agregar clientes " << endl;
 	cout << "2. Agregar productos " << endl;
-	cout << "3. Salir" << endl;
+	cout << "3.Historial " << endl;
+    cout << "4. Salir" << endl;
 	cin >> edilson_temp;
-	while(edilson_temp<1 || edilson_temp>3){
+	while(edilson_temp<1 || edilson_temp>4){
 		cout << "Error, ingrese una opcion del menu..." << endl;
 		cout << "1. Agregar clientes " << endl;
 		cout << "2. Agregar productos " << endl;
-		cout << "3. Salir" << endl;
+        cout << "3.Historial " << endl;
+		cout << "4. Salir" << endl;
 		cin >> edilson_temp;
 	}
 	return edilson_temp;
@@ -102,4 +117,19 @@ int Edilson_PMenu(){
 		cin >> edilson_temp;
 	}
 	return edilson_temp;
+}
+
+void Edilson_Historial(vector<Productos*> morazan_list){
+    for(int i=0;i<morazan_list.size();i++){
+        cout<<morazan_list->toString()<<endl;
+        cout<<"Tipo de Producto : ";
+        if(morazan_list->getId()==1){
+            cout<<"Educativos"<<endl;
+        }else if(morazan_list->getId()==2){
+            cout<<"Alcoholicos"<<endl;
+        }else if(morazan_list->getId()==3){
+            cout<<"Lujo"<<endl;
+        }
+    }
+
 }
