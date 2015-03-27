040c75abec4d07e6975a2dc8b4c2ec7cb06fdea0
Guillermo LopeZ <guillelopez_13@hotmail.com>
2015-03-27 14:31:07 -0600

    skeleton

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include "mainwindow.h"
 #include <iostream>
 #include <vector>
+#include <string>
+#include "productos.h"
+#include "lujo.h"
+#include "alcoholicos.h"
 using std::cout;
 using std::cin;
 using std::endl;
-int Menu();
+using std::string;
+int Edilson_Menu();
+int Edilson_PMenu();
 int main(int argc, char *argv[])
 {
 	vector<Productos*> productos;
@@ -16,27 +21,85 @@ int main(int argc, char *argv[])
     cout << "Tasa de productos de lujo: ";
     cin >> edilson_delujo;
 
-    switch(Menu()){
+    switch(Edilson_Menu()){
     	case 1:
+    		string edilson_nombre;
+    		cout << "Ingrese el nombre del cliente: ";
+    		cin >> edilson_nombre;
     		break;
     	case 2:
+    		switch(Edilson_PMenu()){
+    			case 1:
+    				double edilson_volumen, edilson_peso, edilson_precio;
+    				string edilson_cliente;
+    				cout << "Volumen: ";
+    				cin >> edilson_volumen;
+    				cout << "Peso: ";
+    				cin >> edilson_peso;
+    				cout << "Cliente: ";
+    				cin >> edilson_cliente;
+    				cout << "Precio :";
+    				cin >> edilson_precio;
+    				break;
+    			case 2:
+    				double edilson_volumen, edilson_peso, edilson_precio;
+    				string edilson_cliente;
+    				cout << "Volumen: ";
+    				cin >> edilson_volumen;
+    				cout << "Peso: ";
+    				cin >> edilson_peso;
+    				cout << "Cliente: ";
+    				cin >> edilson_cliente;
+    				cout << "Precio :";
+    				cin >> edilson_precio;
+    				break;
+    			case 3:
+    				double edilson_volumen, edilson_peso, edilson_precio;
+    				string edilson_cliente;
+    				cout << "Volumen: ";
+    				cin >> edilson_volumen;
+    				cout << "Peso: ";
+    				cin >> edilson_peso;
+    				cout << "Cliente: ";
+    				cin >> edilson_cliente;
+    				cout << "Precio :";
+    				cin >> edilson_precio;dg
+    				break;
+    		}
     		break;
     }
 
 }
 
 int Menu(){
-	int temp;
+	int edilson_temp;
 	cout << "1. Agregar clientes " << endl;
 	cout << "2. Agregar productos " << endl;
 	cout << "3. Salir" << endl;
-	cin >> temp;
-	while(temp<1 || temp>2){
+	cin >> edilson_temp;
+	while(edilson_temp<1 || edilson_temp>3){
 		cout << "Error, ingrese una opcion del menu..." << endl;
 		cout << "1. Agregar clientes " << endl;
 		cout << "2. Agregar productos " << endl;
 		cout << "3. Salir" << endl;
-		cin >> temp;
+		cin >> edilson_temp;
 	}
-	return temp;
+	return edilson_temp;
+}
+int Edilson_PMenu(){
+	int edilson_temp;
+	cout << "1. Productos educativos" << endl;
+	cout << "2. Productos alcoholicos" << endl;
+	cout << "3. Productos de Lujo" << endl;
+	cout << "4. Salir" << endl;
+	cin >> edilson_temp;
+	while(edilson_temp<1 || edilson_temp>3){
+		cout << "Error, ingrese una opcion del menu..." << endl;
+		cout << "1. Productos educativos" << endl;
+		cout << "2. Productos alcoholicos" << endl;
+		cout << "3. Productos de Lujo" << endl;
+		cout << "4. Salir" << endl;
+		cin >> edilson_temp;
+	}
+	return edilson_temp;
 }
