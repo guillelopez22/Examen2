b8e367f8c3319f4b8e865017e4df22ecf4be9af4
Guillermo LopeZ <guillelopez_13@hotmail.com>
2015-03-27 13:51:18 -0600

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
 #include "mainwindow.h"
-#include <QApplication>
-
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+int Menu();
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+	vector<Productos*> productos;
+    double edilson_educativo, edilson_alcoholico, edilson_delujo;
+    cout << "Tasa de productos educativos: " ;
+    cin >> edilson_educativo;
+    cout << "Tasa de productos alcoholicos: ";
+    cin >> edilson_alcoholico;
+    cout << "Tasa de productos de lujo: ";
+    cin >> edilson_delujo;
 
-    return a.exec();
+    switch(Menu()){
+    	case 1:
+    		break;
+    	case 2:
+    		break;
+    }
+
+}
+
+int Menu(){
+	int temp;
+	cout << "1. Agregar clientes " << endl;
+	cout << "2. Agregar productos " << endl;
+	cout << "3. Salir" << endl;
+	cin >> temp;
+	while(temp<1 || temp>2){
+		cout << "Error, ingrese una opcion del menu..." << endl;
+		cout << "1. Agregar clientes " << endl;
+		cout << "2. Agregar productos " << endl;
+		cout << "3. Salir" << endl;
+		cin >> temp;
+	}
+	return temp;
 }
