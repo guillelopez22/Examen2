e860811ef5a15e679679818d4972e1a33353afd1
CarvinicioX <cavinicio@yahoo.com>
2015-03-27 15:09:57 -0600

    errores arreglados

--- lujo.cpp
+++ lujo.cpp
@@ -9,13 +9,13 @@ using std::stringstream;
 Lujo::Lujo(double edilson_volumen, double edilson_peso, string edilson_cliente, double edilson_precio, double edilson_tasa):Productos(edilson_volumen, edilson_peso, edilson_cliente, edilson_precio) {
 }
 
-virtual string Lujo::toString()const {
+string Lujo::toString()const {
 	stringstream ss;
 	ss << Productos::toString() << ", Tasa: " << edilson_tasa;
 	return ss.str();
 }
 
-virtual double Lujo::cobrarImpuesto() {
+double Lujo::cobrarImpuesto() {
 	return ((Productos::getEdilson_peso()*this->edilson_tasa + Productos::getEdilson_volumen())*this->edilson_tasa);
 }
 
@@ -23,6 +23,6 @@ void Lujo::setEdilson_tasa(double edilson_tasa) {
 	this->edilson_tasa = edilson_tasa;
 }
 
-virtual int Lujo::getId() {
+int Lujo::getId() {
 	return 3;
 }
