e63914a08e1fa5d8bf578ba93dad55975a49f3f7
Guillermo LopeZ <guillelopez_13@hotmail.com>
2015-03-27 16:20:44 -0600

    asd

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "productos.h"
 #include "lujo.h"
+#include "educativos.h"
 #include "alcoholicos.h"
 using std::vector;
 using std::cout;
@@ -11,13 +12,21 @@ using std::endl;
 using std::string;
 
 int Edilson_Menu();
+string createCliente();
 int Edilson_PMenu();
+string getCliente(vector<string>);
 void Edilson_Historial(vector<Productos*>);
 
 int main(int argc, char *argv[])
 {
+	int seguir=9;
     cout<<"¡Bienvenido Edilson!"<<endl;
 	vector<Productos*> productos;
+	vector<string> clientes;
+	cout << "Antes de empezar, ocupamos por lo menos 5 clientes: " << endl;
+	while(clientes.size()<5){
+		clientes.push_back(createCliente());
+	}
     double edilson_educativo, edilson_alcoholico, edilson_delujo;
     cout << "Tasa de productos educativos: " ;
     cin >> edilson_educativo;
@@ -25,71 +34,78 @@ int main(int argc, char *argv[])
     cin >> edilson_alcoholico;
     cout << "Tasa de productos de lujo: ";
     cin >> edilson_delujo;
-
-    switch(Edilson_Menu()){
-    	case 1:{
-    		string edilson_nombre;
-    		cout << "Ingrese el nombre del cliente: ";
-    		cin >> edilson_nombre;
-    		}
-    		break;
-    	case 2:{
-    		double edilson_volumen, edilson_peso, edilson_precio;
-    		string edilson_cliente;
-    		switch(Edilson_PMenu()){
-    			case 1:{
-    				cout << "Volumen: ";
-    				cin >> edilson_volumen;
-    				cout << "Peso: ";
-    				cin >> edilson_peso;
-    				cout << "Cliente: ";
-    				cin >> edilson_cliente;
-    				cout << "Precio :";
-    				cin >> edilson_precio;
-    			}
-    				break;
-    			
-    			case 2:{
-    				cout << "Volumen: ";
-    				cin >> edilson_volumen;
-    				cout << "Peso: ";
-    				cin >> edilson_peso;
-    				cout << "Cliente: ";
-    				cin >> edilson_cliente;
-    				cout << "Precio :";
-    				cin >> edilson_precio;
-    			}	
-    				break;
-    		
-    			case 3:{
-    				cout << "Volumen: ";
-    				cin >> edilson_volumen;
-    				cout << "Peso: ";
-    				cin >> edilson_peso;
-    				cout << "Cliente: ";
-    				cin >> edilson_cliente;
-    				cout << "Precio :";
-    				cin >> edilson_precio;
-    				}
-    				break;
-    			case 4:{
-    				break;
-    			}
-    			break;
-    			
-    		}
-    	}
-    		break;
-            case 3 : {
-                Edilson_Historial(productos);
-            }
-            break;
-            case 4:{
-                break;
-            }
-            break;
-    }
-
+    while(seguir != 4){
+	    switch(Edilson_Menu()){
+	    	case 1:{
+	    		string edilson_nombre;
+	    		cout << "Ingrese el nombre del cliente: ";
+	    		cin >> edilson_nombre;
+	    		clientes.push_back(edilson_nombre);
+	    		}
+	    		break;
+	    	case 2:{
+	    		double edilson_volumen, edilson_peso, edilson_precio;
+	    		string edilson_cliente;
+	    		switch(Edilson_PMenu()){
+	    			case 1:{
+	    				cout << "Volumen: ";
+	    				cin >> edilson_volumen;
+	    				cout << "Peso: ";
+	    				cin >> edilson_peso;
+	    				edilson_cliente = getCliente(clientes);
+	    				cout << "Precio :";
+	    				cin >> edilson_precio;
+	    				productos.push_back(new Educativos(edilson_volumen, edilson_peso, edilson_cliente, edilson_precio, edilson_educativo));
+	    			}
+	    				break;
+	    			
+	    			case 2:{
+	    				cout << "Volumen: ";
+	    				cin >> edilson_volumen;
+	    				cout << "Peso: ";
+	    				cin >> edilson_peso;
+	    				edilson_cliente = getCliente(clientes);
+	    				cout << "Precio :";
+	    				cin >> edilson_precio;
+	    				productos.push_back(new Alcoholicos(edilson_volumen, edilson_peso, edilson_cliente, edilson_precio, edilson_educativo));
+	    			}	
+	    				break;
+	    		
+	    			case 3:{
+	    				cout << "Volumen: ";
+	    				cin >> edilson_volumen;
+	    				cout << "Peso: ";
+	    				cin >> edilson_peso;
+	    				edilson_cliente = getCliente(clientes);
+	    				cout << "Precio :";
+	    				cin >> edilson_precio;
+	    				productos.push_back(new Lujo(edilson_volumen, edilson_peso, edilson_cliente, edilson_precio, edilson_educativo));
+	    				}
+	    				break;
+	    			case 4:{
+	    				break;
+	    			}
+	    			break;
+	    			
+	    		}
+	    	}
+	    		break;
+	            case 3 : {
+	                Edilson_Historial(productos);
+	            }
+	            break;
+	            case 4:{
+	            	seguir=4;
+	                break;
+	            }
+	            break;
+	    }
+	}
+	for (int i = 0; i < productos.size(); ++i)
+	{
+		delete productos.at(i);
+	}
+	return 0;
 }
 
 int Edilson_Menu(){
@@ -130,14 +146,40 @@ int Edilson_PMenu(){
 void Edilson_Historial(vector<Productos*> morazan_list){
     for(int i=0;i<morazan_list.size();i++){
         cout<<morazan_list.at(i)->toString()<<endl;
-        cout<<"Tipo de Producto : ";
+        cout<<"Tipo de Impuesto : ";
         if(morazan_list.at(i)->getId()==1){
-            cout<<"Educativos"<<endl;
+            cout<<"Educativo"<<endl;
         }else if(morazan_list.at(i)->getId()==2){
-            cout<<"Alcoholicos"<<endl;
+            cout<<"Alcoholico"<<endl;
         }else if(morazan_list.at(i)->getId()==3){
             cout<<"Lujo"<<endl;
         }
+        cout << "El Impuesto: " << morazan_list.at(i)->cobrarImpuesto() << endl;
     }
 
+}
+string getCliente(vector<string> clientes){
+	cout << endl;
+	cout << "Cliente que lo compra: " << endl;
+	int n;
+	for (int i = 0; i < clientes.size(); ++i)
+	{
+		cout << i << ": " << clientes.at(i) << endl;
+	}
+	cin >>n;
+	while( n<0 || n>clientes.size() ){
+		cout << "Error... ingrese uno dentro de la lista..." << endl;
+		for (int i = 0; i < clientes.size(); ++i){
+		cout << i << ": " << clientes.at(i) << endl;
+		}
+	cin >>n;
+	}
+	return clientes.at(n);
+}
+string createCliente(){
+	string temp;
+	cout << "nombre: ";
+	cin >> temp;
+	
+	return temp;
 }
